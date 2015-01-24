b8d1f8ebb31424944cb734dbaceeb8337c474cc8
Cholwich Nattee <cholwich@gmail.com>
2015-01-24 14:22:51 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/tu/siit/its333/lab2exercise1/MainActivity.java
@@ -0,0 +1,83 @@
+package th.ac.tu.siit.its333.lab2exercise1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // expr = the current string to be calculated
+    StringBuffer expr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        expr = new StringBuffer();
+        updateExprDisplay();
+    }
+
+    public void updateExprDisplay() {
+        TextView tvExpr = (TextView)findViewById(R.id.tvExpr);
+        tvExpr.setText(expr.toString());
+    }
+
+    public void recalculate() {
+        //Calculate the expression and display the output
+    }
+
+    public void digitClicked(View v) {
+        //d = the label of the digit button
+        String d = ((TextView)v).getText().toString();
+        //append the clicked digit to expr
+        expr.append(d);
+        //update tvExpr
+        updateExprDisplay();
+        //calculate the result if possible and update tvAns
+        recalculate();
+    }
+
+    public void operatorClicked(View v) {
+        //IF the last character in expr is not an operator and expr is not "",
+        //THEN append the clicked operator and updateExprDisplay,
+        //ELSE do nothing
+    }
+
+    public void ACClicked(View v) {
+        //Clear expr and updateExprDisplay
+    }
+
+    public void BSClicked(View v) {
+        //Remove the last character from expr, and updateExprDisplay
+        if (expr.length() > 0) {
+            expr.deleteCharAt(expr.length()-1);
+            updateExprDisplay();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
